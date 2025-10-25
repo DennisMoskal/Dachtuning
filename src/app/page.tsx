ce8dd35f61ed4fe30dccae779015d55790f92b12
DennisMoskal <dennismoskal@yahoo.com>
2025-10-25 02:15:56 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { Play, Home, Shield, Droplet, Paintbrush, Building2, Sun, PhoneCall, Mail, MapPin, Clock, Wrench, Users, Award, Sparkles, Droplets, CheckCircle, Star } from "lucide-react";
 import { useState } from "react";
 
